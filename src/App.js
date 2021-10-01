3ab0c5605bcd9da43cbd93cce9be942864b0a256
gladishd <gladish.dean@gmail.com>
2021-09-30 23:51:51 -0500

    Uses npx create-react-app, on the topic not of user experience but of development; to contribute, other developers should have a good code editor like VSCode, iTerm, and be familiar with installing Git on macOS via Xcode Command Line Tools.  The great thing about Create React App is that for the sake of code hygiene it performs npm install dependencies.  We can host on localhost :)!  After heroku auth:whoami, does heroku create, and puts task.md in root folder.  Adds question addition problem, input box, and refactors functional component to class component for responsiveness.  Includes mouseDown function with this binding, which allows proper documentation for the existence of the mouseDown function.  * -> *, - -> -, / -> %2F, . -> ., space -> %20, ^ -> %5E, = -> %3D, [ -> %5B, is retrieved by encodeURIComponent(`{expression}`).  While using the link types noopener, the keyword for the rel attribute of the a href element, which instructs the browser to navigate to the target resource without granting the new browsing context access to the document that opened it, using noreferrer in order not to provide the Referer HTTP header, with target = "_blank" to open in a new tab, refactors the existing Create React App code so that the math question rotates by using the className App-logo as its container div, which really helps with the user experience; users prefer responsive, non-interfering designs which animate what matters.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      userInput: '',
+    }
+    this.mouseDown = this.mouseDown.bind(this)
+  }
+  mouseDown = function (e) {
+    e.preventDefault()
+    console.log("the mouse is down!")
+  }
+  async componentDidMount() {
+    // data:
+    //   {
+    //     "expr": [
+    //       "a = 1.2 * (2 + 4.5)",
+    //       "a / 2",
+    //       "5.08 cm in inch",
+    //       "sin(45 deg) ^ 2",
+    //       "9 / 3 + 2i",
+    //       "b = [-1, 2; 3, 1]",
+    //       "det(b)"
+    //     ],
+    //     "precision": 14
+    //   }
+
+    console.log("the component mounts")
+
+    let query = encodeURIComponent('sin(45 deg) ^ 2')
+    let response = await axios({
+      method: 'get',
+      url: `http://api.mathjs.org/v4/?expr=${query}`,
+
+    }).then(function (response) {
+      console.log('the response is ', response);
+    }).catch(function (error) {
+      console.log(error)
+    }).then(function () {
+      //always executed
+      console.log("alert!")
+    })
+
+    console.log('response is ', response)
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <p className="App-logo" alt="Mathematical Expression">
+            <code>A + B = </code> ?
+          </p>
+          <input />
+          <a
+            className="App-link"
+            href="https://api.mathjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            This project uses the Math.js API!
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
