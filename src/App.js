9a96633e59945ca2bb702b3bf56fe7dac7293dd2
gladishd <gladish.dean@gmail.com>
2021-10-01 02:26:36 -0500

    Let's add more to this planning.md documentation.  Instead of using event listeners and the DOM, integrates swal with method chaining within which the axios GET request is made per the axios docs, which does error catching and handling to submit and return messages from the api.mathjs.org API, instead of modifying DOM elements when events' listeners are triggered on them, uses axios.get method chaining to handle server response in a .then/.catch/.then block which works with the promised response, and by the way the user and the soon-to-be event handler will work with user input!  Installs React Bootstrap to include React Bootstrap and Button from 'react-bootstrap/Button', styling button with type=submit to follow React Bootstrap guidelines, modeling button after react-bootstrap.github.io/components/buttons.  e.preventDefault is on the componentDidMount turned into async onSubmit(e) {... for which there's an event handler written in JSX!  Optimizes submit flexbox for mobile view @media only screen and (max-width: 600px) {... and fancifies button with moving CSS background, displays previous user input and API responses from Math.js with the setResponseData helper function transferred from _this_.  Includes non-typing equality check!  ::placeholder is colored:).  Every element of the styling is superbly done.

--- src/App.js
+++ src/App.js
@@ -1,63 +1,138 @@
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import React from 'react';
-
+import swal from 'sweetalert';
+import { Form } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button'
 
 export default class App extends React.Component {
   constructor() {
     super()
     this.state = {
+      a: null,
+      operation: '',
+      b: null,
+      expression: '',
       userInput: '',
+      responseData: [],
     }
-    this.mouseDown = this.mouseDown.bind(this)
+    this.maintainHistory = this.maintainHistory.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+    this.generateMathProblem = this.generateMathProblem.bind(this)
   }
-  mouseDown = function (e) {
-    e.preventDefault()
-    console.log("the mouse is down!")
-  }
-  async componentDidMount() {
-    // data:
-    //   {
-    //     "expr": [
-    //       "a = 1.2 * (2 + 4.5)",
-    //       "a / 2",
-    //       "5.08 cm in inch",
-    //       "sin(45 deg) ^ 2",
-    //       "9 / 3 + 2i",
-    //       "b = [-1, 2; 3, 1]",
-    //       "det(b)"
-    //     ],
-    //     "precision": 14
-    //   }
-
-    console.log("the component mounts")
-
-    let query = encodeURIComponent('sin(45 deg) ^ 2')
-    let response = await axios({
-      method: 'get',
-      url: `http://api.mathjs.org/v4/?expr=${query}`,
-
-    }).then(function (response) {
-      console.log('the response is ', response);
-    }).catch(function (error) {
-      console.log(error)
-    }).then(function () {
-      //always executed
-      console.log("alert!")
+  generateMathProblem() {
+    let a = Math.floor(Math.random() * 101)
+    let operation = ['+', '-', '*', '/'][Math.floor(Math.random() * 4)]
+    let b = Math.floor(Math.random() * 101)
+    let expression = `${a} ${operation} ${b}`
+    this.setState({
+      a, operation, b, expression
     })
+    this.maintainHistory(`${expression} = ?`)
+  }
+  componentDidMount() {
+    try {
+      this.generateMathProblem()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  maintainHistory = function (data) {
+    try {
+      this.setState({
+        responseData: [...this.state.responseData, data]
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  async onSubmit(e) {
+    try {
+      e.preventDefault()
+      let value = e.target.elements.submittedAnswer.value
+      const { maintainHistory, generateMathProblem } = this;
+      maintainHistory(value)
+      let expression = encodeURIComponent(this.state.expression)
 
-    console.log('response is ', response)
+      swal({
+        title: `Your answer is ${value}.`,
+        text: `Do you want to submit the value ${value}?`,
+        icon: 'warning',
+        buttons: true,
+        dangerMode: true,
+      }).then(async (submitAnswer) => {
+        if (submitAnswer) {
+          await axios({
+            method: 'get',
+            url: `http://api.mathjs.org/v4/?expr=${expression}`,
+          }).then(function (response) {
+            // eslint-disable-next-line
+            if (value == response.data) {
+              swal(`Thank you for submitting such a correct query and query format.  The response is ${response.data}`)
+              generateMathProblem()
+            } else {
+              swal(`The server has spoken, you didn't submit the correct response!`)
+            }
+          }).catch(function (error) {
+            console.log(error)
+          })
+        } else {
+          swal("Excellent!  Try a more complete and fun answer which matches one of the displayed options.")
+        }
+      })
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   render() {
     return (
       <div className="App" >
         <header className="App-header">
+          <div className='titleText'>Addition Practice</div>
+          <div style={{ textAlign: 'left', display: 'flex', width: '90vw', justifyContent: 'space-between' }}>
+            <div>
+              <b>A list of example expressions is,</b>
+              <br />
+              a = 1.2 * (2 + 4.5),
+              <br />
+              a / 2,
+              <br />
+              5.08 cm in inch,
+              <br />
+              sin(45 deg) ^ 2,
+              <br />
+              9 / 3 + 2i,
+              <br />
+              b = [-1, 2; 3, 1],
+              <br />
+              det(b)
+            </div><div
+              style={{ display: 'flex', flexDirection: 'column' }}
+            >Previous Response Values!
+              {this.state.responseData.map(e => <div>{e}</div>)}</div>
+          </div>
           <p className="App-logo" alt="Mathematical Expression">
-            <code>A + B = </code> ?
+            <code>{this.state.expression} = ?</code>
           </p>
-          <input />
+          <Form style={{ position: 'absolute' }} onSubmit={this.onSubmit}>
+            <Form.Group role="form" controlId="form.ControlTextArea"
+            >
+              <div style={{ display: 'flex' }}
+                id='formFlexBox'
+              >
+                <Form.Control as="textarea"
+                  className='appClassName'
+                  name='submittedAnswer'
+                  placeholder='Give the API a value!  The soon-to-be value will be much appreciated!' rows={3} style={{ width: '50vw' }} />
+
+                <Button className='submitButton' variant="primary" type="submit">
+                  Submit Answer
+                </Button>
+              </div>
+            </Form.Group>
+          </Form>
+
           <a
             className="App-link"
             href="https://api.mathjs.org/"
@@ -67,6 +142,7 @@ export default class App extends React.Component {
             This project uses the Math.js API!
           </a>
         </header>
+
       </div>
     );
   }
